f52777b9831ca4440715c6b971fb070a91c0673f
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-03-05 18:37:58 +0100

    divide code for smaller components

--- src/App.js
+++ src/App.js
@@ -6,34 +6,13 @@ import Header from './components/Header'
 function App() {
   const [currencies, setCurrencies] = useState([])
   const [lastUpdate, setlastUpdate] = useState('')
-  const [lastMonthCurrencies, setLastMonthCurrencies] = useState([])
-  const [currenciesOldData, setcurrenciesOldData] = useState([])
-
-  useEffect(()=>{
-    let dateNow = getCurrentDate()
-    let monthEalier = getMonthEalierDate()
-    fetch(`https://api.nbp.pl/api/exchangerates/tables/a/${monthEalier}/${dateNow}/`)
-    .then(res => {
-      if(res.ok){
-        return res.json()
-      }else{
-        return Promise.reject(res)
-      }
-    })
-    .then(res=>{
-      setLastMonthCurrencies(res)
-    })    
-    .catch(error => {
-      console.error("Błąd pobrania API!")
-    })
-  }, [])
 
   useEffect(() => {
     fetchData()
     setInterval(() => {
       fetchData()
     }, 10000);
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const fetchData = () =>{
     fetch(`https://api.nbp.pl/api/exchangerates/tables/c/`)
@@ -70,41 +49,12 @@ function App() {
     return date
   }
 
-  const getMonthEalierDate = () =>{
-    let currentdate = getCurrentDate()
-    let currentMonth = +currentdate.slice(5,7)
-    let monthEalier = --currentMonth < 10 ? '0' + currentMonth : currentMonth
-    let monthEalierDate = `${currentdate.slice(0,5).concat(monthEalier).concat(currentdate.slice(7))}`
-    return monthEalierDate
-  }
-// Pobieram wszystkie elementy tablicy i zwracam tablice, gdzie pierwszy element posiada tablice trzech
-// walut (EUR, USD, GBP), a drugi element datę.
-// dzięki temu w prosty sposób mogę zmieniac, które waluty mają być wyświetlane
-  const convertLastMonthCurrencies = () =>{
-    currenciesOldData.length = 0;
-    currenciesOldData.push(['Dzień','EUR','USD','GBP'])
-    lastMonthCurrencies.map(item=>{
-      return [item.rates.filter(currency =>{
-        if(currency.code === 'EUR' || currency.code === 'USD' || currency.code === 'GBP'){
-          return true;
-        }else{
-          return false;
-        }
-      }), item.effectiveDate]
-    }).forEach(currency =>{
-      currenciesOldData.push([currency[1], currency[0][0].mid, currency[0][1].mid, currency[0][2].mid])
-    })
-  }
-
-  useEffect(()=>{
-    convertLastMonthCurrencies()
-  },[lastMonthCurrencies])
 
 
   return (
     <div className="App">
       <Header lastUpdate={lastUpdate} currencies={currencies} />
-      <DrawChart currenciesOldData={currenciesOldData} />
+      <DrawChart getCurrentDate={getCurrentDate} />
     </div>
   );
 }
--- src/components/DrawChart/index.js
+++ src/components/DrawChart/index.js
@@ -1,7 +1,63 @@
+import { useState, useEffect } from 'react';
+
 import Chart from "react-google-charts";
 import './style.scss'
 
 const DrawChart = (props) =>{
+    const [lastMonthCurrencies, setLastMonthCurrencies] = useState([])
+    const [currenciesOldData, setcurrenciesOldData] = useState([])
+
+    useEffect(()=>{
+        let dateNow = props.getCurrentDate()
+        let monthEalier = getMonthEalierDate()
+        fetch(`https://api.nbp.pl/api/exchangerates/tables/a/${monthEalier}/${dateNow}/`)
+        .then(res => {
+          if(res.ok){
+            return res.json()
+          }else{
+            return Promise.reject(res)
+          }
+        })
+        .then(res=>{
+          setLastMonthCurrencies(res)
+        })    
+        .catch(error => {
+          console.error("Błąd pobrania API!")
+        })
+      }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+      const getMonthEalierDate = () =>{
+        let currentdate = props.getCurrentDate()
+        let currentMonth = +currentdate.slice(5,7)
+        let monthEalier = --currentMonth < 10 ? '0' + currentMonth : currentMonth
+        let monthEalierDate = `${currentdate.slice(0,5).concat(monthEalier).concat(currentdate.slice(7))}`
+        return monthEalierDate
+      }
+
+      // Pobieram wszystkie elementy tablicy i zwracam tablice, gdzie pierwszy element posiada tablice trzech
+    // walut (EUR, USD, GBP), a drugi element datę.
+    // dzięki temu w prosty sposób mogę zmieniac, które waluty mają być wyświetlane
+    const convertLastMonthCurrencies = () =>{
+        currenciesOldData.length = 0;
+        currenciesOldData.push(['Dzień','EUR','USD','GBP'])
+        lastMonthCurrencies.map(item=>{
+        return [item.rates.filter(currency =>{
+            if(currency.code === 'EUR' || currency.code === 'USD' || currency.code === 'GBP'){
+            return true;
+            }else{
+            return false;
+            }
+        }), item.effectiveDate]
+        }).forEach(currency =>{
+        currenciesOldData.push([currency[1], currency[0][0].mid, currency[0][1].mid, currency[0][2].mid])
+        })
+    }
+    
+    
+    useEffect(()=>{
+        convertLastMonthCurrencies()
+    },[lastMonthCurrencies]) // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
         <div className="chart">
             <Chart
@@ -9,7 +65,7 @@ const DrawChart = (props) =>{
                 height={'400px'}
                 chartType="Line"
                 loader={<div>Loading Chart</div>}
-                data={props.currenciesOldData}
+                data={currenciesOldData}
                 options={{
                     chart: {
                     title: 'Wykres średnich kursów walut',
